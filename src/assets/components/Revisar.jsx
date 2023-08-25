7bcd5547f66f5296d0b7cfc73b6de211da055910
Vicki Avola <vavola@comunyt.co>
2023-08-25 07:05:37 +0200

    Estructura del Revisar

--- src/assets/components/Revisar.jsx
+++ src/assets/components/Revisar.jsx
@@ -1,9 +1,59 @@
+import { useState, useEffect } from 'react';
+
 export const Revisar = () => {
 
+     <p>Revisar</p>
+
      return (
-          <div>
-               revisar
+     <>
+
+<div className="bg-light" style={{marginTop:20, padding:20}}>
+
+    <div className="h3">
+      Estado de cuenta
+    </div>
+
+    <div className="table-responsive">
+      
+
+
+        <div >
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Compras realizadas</h5>
+                <p className="card-text"> TEXTO A REEMPLAZAR </p>
+              </div>
+            </div>
           </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+              <h5 className="card-title">Ventas realizadas</h5>
+                <p className="card-text"> TEXTO A REEMPLAZAR </p>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Saldo</h5>
+                <p className="card-text"> TEXTO A REEMPLAZAR </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      
+    </div>
+ 
+  </div>
+
+
+
+     </> 
 
 )
 }
